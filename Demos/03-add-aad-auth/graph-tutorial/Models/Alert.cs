a4d36cabffe3f5932df95539a3f44871a6d62332
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-23 13:37:24 -0400

    Copied completed module 1 project to module 3

---
+++ Demos/03-add-aad-auth/graph-tutorial/Models/Alert.cs
@@ -0,0 +1,10 @@
+﻿namespace graph_tutorial.Models
+{
+    public class Alert
+    {
+        public const string AlertKey = "TempDataAlerts";
+
+        public string Message { get; set; }
+        public string Debug { get; set; }
+    }
+}
