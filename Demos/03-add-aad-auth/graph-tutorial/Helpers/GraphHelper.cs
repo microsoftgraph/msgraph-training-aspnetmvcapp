09d8b0a3b33b805308446cfbbba26b03963e9b83
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-24 10:24:39 -0400

    Completed module 3

---
+++ Demos/03-add-aad-auth/graph-tutorial/Helpers/GraphHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Graph;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace graph_tutorial.Helpers
+{
+    public class GraphHelper
+    {
+        public static async Task<User> GetUserDetailsAsync(string accessToken)
+        {
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization = 
+                            new AuthenticationHeaderValue("Bearer", accessToken);
+                    }));
+
+            return await graphClient.Me.Request().GetAsync();
+        }
+    }
+}
