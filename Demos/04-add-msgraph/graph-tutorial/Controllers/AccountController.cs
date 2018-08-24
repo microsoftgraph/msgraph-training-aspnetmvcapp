0c6ca984e68eefbdc94bc28e94b6b7f00e412717
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-08-24 11:39:32 -0400

    Copied completed module 3 project to module 4

---
+++ Demos/04-add-msgraph/graph-tutorial/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Web;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure
+                Request.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+    }
+}
---
+++ Demos/04-add-msgraph/graph-tutorial/Controllers/BaseController.cs
@@ -0,0 +1,53 @@
+﻿using graph_tutorial.Models;
+using graph_tutorial.TokenStorage;
+using Microsoft.Owin.Security.Cookies;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Get the signed in user's id and create a token cache
+                string signedInUserId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+                SessionTokenStore tokenStore = new SessionTokenStore(signedInUserId, HttpContext);
+
+                if (tokenStore.HasData())
+                {
+                    // Add the user to the view bag
+                    ViewBag.User = tokenStore.GetUserDetails();
+                }
+                else
+                {
+                    // The session has lost data. This happens often
+                    // when debugging. Log out so the user can log back in
+                    Request.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+                    filterContext.Result = RedirectToAction("Index", "Home");
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected void Flash(string message, string debug=null)
+        {
+            var alerts = TempData.ContainsKey(Alert.AlertKey) ?
+                (List<Alert>)TempData[Alert.AlertKey] :
+                new List<Alert>();
+
+            alerts.Add(new Alert
+            {
+                Message = message,
+                Debug = debug
+            });
+
+            TempData[Alert.AlertKey] = alerts;
+        }
+    }
+}
