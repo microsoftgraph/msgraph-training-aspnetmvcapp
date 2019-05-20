848431e4dbb14534934940a6c85f21ce3bf66b3a
Rob Windsor <rob.windsor@gmail.com>
2019-05-20 12:50:53 -0400

    Update instructions for get calendar data part of tutorial

--- Demos/03-add-msgraph/graph-tutorial/Controllers/CalendarController.cs
+++ Demos/03-add-msgraph/graph-tutorial/Controllers/CalendarController.cs
@@ -14,6 +14,16 @@ namespace graph_tutorial.Controllers
         public async Task<ActionResult> Index()
         {
             var events = await GraphHelper.GetEventsAsync();
+
+
+            foreach (var ev in events)
+            {
+                ev.Start.DateTime = DateTime.Parse(ev.Start.DateTime).ToLocalTime().ToString();
+                ev.Start.TimeZone = TimeZoneInfo.Local.Id;
+                ev.End.DateTime = DateTime.Parse(ev.End.DateTime).ToLocalTime().ToString();
+                ev.End.TimeZone = TimeZoneInfo.Local.Id;
+            }
+
             return View(events);
         }
     }
