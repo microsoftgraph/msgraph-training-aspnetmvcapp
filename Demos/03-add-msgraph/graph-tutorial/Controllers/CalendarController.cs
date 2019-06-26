4b8460812b8368379c9b154f4aa5cfe3e7179144
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-06-26 16:18:59 -0400

    Added MS graph

---
+++ Demos/03-add-msgraph/graph-tutorial/Controllers/CalendarController.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using graph_tutorial.Helpers;
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    public class CalendarController : BaseController
+    {
+        // GET: Calendar
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+            var events = await GraphHelper.GetEventsAsync();
+
+            // Change start and end dates from UTC to local time
+            foreach (var ev in events)
+            {
+                ev.Start.DateTime = DateTime.Parse(ev.Start.DateTime).ToLocalTime().ToString();
+                ev.Start.TimeZone = TimeZoneInfo.Local.Id;
+                ev.End.DateTime = DateTime.Parse(ev.End.DateTime).ToLocalTime().ToString();
+                ev.End.TimeZone = TimeZoneInfo.Local.Id;
+            }
+
+            return View(events);
+        }
+    }
+}
