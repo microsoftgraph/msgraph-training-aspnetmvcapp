14ddb9df86987371d1a5ad2592e8e3bba91eda32
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-06-26 13:28:01 -0400

    Added copyright/license

--- Demos/01-create-app/graph-tutorial/Controllers/BaseController.cs
+++ Demos/01-create-app/graph-tutorial/Controllers/BaseController.cs
@@ -1,4 +1,7 @@
-﻿using graph_tutorial.Models;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using graph_tutorial.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
--- Demos/02-add-aad-auth/graph-tutorial/Models/Alert.cs
+++ Demos/02-add-aad-auth/graph-tutorial/Models/Alert.cs
@@ -1,4 +1,7 @@
-﻿namespace graph_tutorial.Models
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace graph_tutorial.Models
 {
     // Used to flash error messages in the app's views.
     public class Alert
