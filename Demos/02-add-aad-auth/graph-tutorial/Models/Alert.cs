c92de232f4c9e2f37ec81b684ae24ccae00c4227
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-06-26 13:10:33 -0400

    Copied new 01 project to 02 folder

---
+++ Demos/02-add-aad-auth/graph-tutorial/Models/Alert.cs
@@ -0,0 +1,10 @@
+﻿namespace graph_tutorial.Models
+{
+    // Used to flash error messages in the app's views.
+    public class Alert
+    {
+        public const string AlertKey = "TempDataAlerts";
+        public string Message { get; set; }
+        public string Debug { get; set; }
+    }
+}
