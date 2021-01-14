f7ed40cecbdc56ef637a6c0c8c63166c32aa4d6c
Andrew Connell <me@andrewconnell.com>
2021-01-14 10:47:05 -0500

    Add copyright, addressing reviewer feedback)

--- Demos/01-create-app/graph-tutorial/Startup.cs
+++ Demos/01-create-app/graph-tutorial/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Owin;
 using Owin;
 using System;
 using System.Threading.Tasks;
